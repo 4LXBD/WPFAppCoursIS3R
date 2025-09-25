5a09a2b3ec7e8dadea32ee8660941607a67a4164
Alex Baescu <baescualexandru17.gmail.com>
2025-09-25 11:48:45 +0200

    Application to send mails

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,30 @@ namespace WPFApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var mail = new MailMessage(FromBox.Text, ToBox.Text, SubjectBox.Text, BodyBox.Text);
+
+
+                using (var client = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    client.Credentials = new NetworkCredential(FromBox.Text, PasswordBox.Password);
+                    client.EnableSsl = true;
+
+                    await client.SendMailAsync(mail);
+                }
+
+                StatusBlock.Text = "Email sent successfully!";
+            }
+
+            catch (System.Exception ex)
+            {
+                StatusBlock.Text = $"Error: {ex.Message}";
+            }
         }
     }
 }
